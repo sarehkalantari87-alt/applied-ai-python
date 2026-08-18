614f238ab58573ec7860afc0daa6cf11f2997571
sarehkalantari87-alt <sareh.kalantari87@gmail.com>
2026-08-18 14:11:49 +0330

    Add files via upload

---
+++ meeting_1_student.py
@@ -0,0 +1,62 @@
+# تعریف متغیر و مقدار دهی و چاپ کردن
+# a=10
+# b=14
+# print(a+b)
+# //////////////////////////
+#  گرفتن مقدار از ورودی از نوع صحیح
+# a=int(input('enter a number:'))
+# b=int(input('enter another number:'))
+# print(a+b)
+# //////////////////////////
+#  گرفتن مقدار از ورودی از نوع اعشاری
+# a=float(input("Enter a number:"))
+# b=float(input("Enter another number:"))
+# print(a+b)
+# //////////////////////////
+#  گرفتن مقدار از ورودی از نوع رشته
+# a=input('enter a string:')
+# b=input('enter another string:')
+# print(a+b)
+# //////////////////////////
+# برنامه ماشین حساب ساده
+# a=int(input('enter one number:'))
+# b=int(input('enter tow number:'))
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a**b)
+# //////////////////////////
+#  محاسبه True(درست) و False(غلط) و in(وجود دارد) و not in(وجود ندارد)
+# a='This is a Class Python'
+# print('p' in a)
+# print('i' in a)
+# print('A' not in a)
+# //////////////////////////
+#بررسی کردن True(درست) و False(غلط) در is(وجود دارد) و is not (وجود ندارد)
+# a=5
+# b=15
+# print(a is b)
+# print(a is not b)
+# //////////////////////////
+# برنامه ای که شعاع دایره را بگیرد محیط و مساحت آن را بدست بیارد
+# r=float(input('enter a reduce:'))
+# pi=3.14
+# a=r**2*pi
+# b=r*2*pi
+# print('masahat is :',a)
+# print('mohit is :',b)
+# //////////////////////////
+# برنامه ای که سن را بگیرد به ثانیه و دقیقه و ساعت تبدیل کند( هر سال برابر با 3.156*10**7)
+# age=int(input('enter a age:'))
+# second=age*3156e7
+# minute=second/60
+# hour=minute/3600
+# print('second is:',second)
+# print('minute is:',minute)
+# print('hour is:',hour)
+# //////////////////////////
+# برنامه ای که دو تا عدد بگیرد به complex تبدیل کند
+# a=int(input('enter a number:'))
+# b=int(input('enter another number:'))
+# print(complex(a,b))
